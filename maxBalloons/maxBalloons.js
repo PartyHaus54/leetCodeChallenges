934f58c642d062137944b42ff34a564e7db67535
Matt Heindel <mheindel94@gmail.com>
2021-09-13 10:29:58 -0700

    (problem) maxBalloons

---
+++ maxBalloons/maxBalloons.js
@@ -0,0 +1,29 @@
+/*
+https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/637/week-2-september-8th-september-14th/3973/
+
+Maximum Number of Balloons
+
+Given a string text, you want to use the characters of text to form as many
+instances of the word "balloon" as possible.
+
+You can use each character in text at most once. Return the maximum number of
+instances that can be formed.
+
+Example 1:
+Input: text = "nlaebolko"
+Output: 1
+
+Example 2:
+Input: text = "loonbalxballpoon"
+Output: 2
+
+Example 3:
+Input: text = "leetcode"
+Output: 0
+*/
+
+const maxBalloons = (str) => {
+  // TODO: implement
+}
+
+module.exports = maxBalloons;
