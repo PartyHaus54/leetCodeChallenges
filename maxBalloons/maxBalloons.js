cecc16279ff65ebf237b3d7de302a94d05cb481f
Russ Lightsey <texanruss3@gmail.com>
2021-09-14 09:44:11 -0700

    maxBalloons completed

--- maxBalloons/maxBalloons.js
+++ maxBalloons/maxBalloons.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-multi-assign */
 /*
 https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/637/week-2-september-8th-september-14th/3973/
 
@@ -23,7 +24,32 @@ Output: 0
 */
 
 const maxBalloons = (str) => {
-  // TODO: implement
-}
+  if (str.length === 0 || str === undefined || typeof str !== 'string') {
+    return 0;
+  }
+  const tracker = {
+    b: 0,
+    a: 0,
+    l: 0,
+    o: 0,
+    n: 0,
+  };
+  str.split('').forEach((letter) => {
+    if (tracker[letter] !== undefined) {
+      if (letter === 'l' || letter === 'o') {
+        tracker[letter] = tracker[letter] += 0.5;
+      } else {
+        tracker[letter] = tracker[letter] += 1;
+      }
+    }
+  });
+  let count = tracker.b;
+  Object.keys(tracker).forEach((letter) => {
+    if (tracker[letter] < count) {
+      count = tracker[letter];
+    }
+  });
+  return count < 1 ? 0 : count;
+};
 
 module.exports = maxBalloons;
