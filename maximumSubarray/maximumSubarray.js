779739f8348b2d05d8e26ea00b6ab3c6eba6509f
Joseph Peck <joe.r.peck@gmail.com>
2022-03-10 21:52:48 -0800

    You have to account for gravity. I was wondering why they didn't do that

---
+++ maximumSubarray/maximumSubarray.js
@@ -0,0 +1,27 @@
+function maxSubArray(nums) {
+    // Never going to start or end with a negative number
+    // Never going to include a span which nets 0
+    let totalMax = 0;
+    for (let index = 0; index < nums.length; index++) {
+        if (nums[index] > 0) {
+            let subIndex = index + 1;
+            let subMax = nums[index];
+            let subTotal = nums[index];
+            while (subTotal > 0 && subIndex < nums.length) {
+                subTotal += nums[subIndex];
+                if (subTotal > subMax) {
+                    subMax = subTotal;
+                }
+                subIndex++;
+            }
+            if (subMax > totalMax) {
+                totalMax = subMax;
+            }
+        }
+    }
+    return totalMax;
+}
+let msr1 = maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]);
+console.assert(msr1 === 6, 'Bad');
+console.log(msr1);
+//# sourceMappingURL=maximumSubarray.js.map
